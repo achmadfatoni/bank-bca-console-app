db055b34242f52a6e041399f6bffbfc1856b4de6
Achmad Fatoni <achmadfatony@gmail.com>
2018-10-24 23:04:54 +0700

    support date range in mutasi

--- src/Mutasi.php
+++ src/Mutasi.php
@@ -5,6 +5,7 @@ namespace Console;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Helper\Table;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class Mutasi extends Command
@@ -13,13 +14,29 @@ class Mutasi extends Command
     {
         $this->setName('mutasi')
             ->setDescription('Cek mutasi rekening');
+
+        $this->addOption('from', 'f', InputOption::VALUE_REQUIRED, 'Dari tanggal.')
+            ->addOption('to', 't', InputOption::VALUE_REQUIRED, 'Ke tanggal');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $output->writeln("cek mutasi rekening...");
+        $fromDate = $input->getOption('from');
+        $toDate = $input->getOption('to');
+
+        if (!empty($toDate) && empty($fromDate)) {
+            $output->writeln('<error>from date value (--from|-f)required</error>');
+            return;
+        }
+
+        if(empty($fromDate) && empty($toDate)) {
+            $fromDate = date('Y-m-d');
+            $toDate = date('Y-m-d');
+        }
+
+        $output->writeln("cek mutasi rekening dari {$fromDate} ke {$toDate}");
         $bcaParser = new BCAParser(getenv('USERNAME'),getenv('PASSWORD'));
-        $transactions = $bcaParser->getListTransaksi('2018-10-01','2018-10-20');
+        $transactions = $bcaParser->getListTransaksi($fromDate,$toDate);
         $bcaParser->logout();
         $table = new Table($output);
         $mappingTransactions = [];
@@ -29,6 +46,11 @@ class Mutasi extends Command
                 $transaction['description'][0],
                 end($transaction['description']),
             ];
+        }
+
+        if(empty($mappingTransactions)) {
+           $output->writeln('<info>Tidak ada transaksi</info>');
+           return;
         }
         $table->setHeaders(['Tanggal', 'Deskripsi', 'Jumlah'])
             ->setRows($mappingTransactions);
